4c3e92aaff10aefd8ef38a76ef57ade4d248c0f5
blingkoala@163.com <blingkoala@163.com>
2018-07-06 19:41:32 +0800

    添加严重级别

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -1,5 +1,6 @@
-import allure
+import allure, pytest
 class Test_001:
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step(title = "第一个测试")
     def test_001(self):
         allure.attach("描述", "描述内容")
